1f5f8eb99ec657afc648a7a99848c6ee5d8b661c
Alma-Sanchez <alma08sanchez@gmail.com>
2016-11-18 11:31:03 -0500

    Added bg images, changed colors

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 var express = require('express');
 var app = express();
-var reload = require('reload')
+var reload = require('reload');
+var path = require('path');
 
 
 app.use(express.static('public'));
 
+app.disable('x-powered-by');
 
 app.get('/', function (req, res) {
-	res.sendFile(__dirname + "/index.html", function(){});
-	
+	res.sendFile(__dirname + "/index.html");
 });
+
 var server = app.listen(8081, function () {
 	var host = server.address().address;
 	var port = server.address().port;
