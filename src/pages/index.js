d7151859c5206db9127c869c55dca2469ee9c6d8
alma-sanchez <alma08sanchez@gmail.com>
2021-03-17 17:39:19 -0700

    Moving over to gatsby instead of 11ty

---
+++ src/pages/index.js
@@ -0,0 +1,42 @@
+import React from "react"
+
+export default function Home() {
+  return <div>
+    Alma's Portfolio
+
+    <div className="intro">
+      My name is Alma, I'm a first generation college graduate with a BS in Computer Science from American University. I love working with JavaScript and learning more about UI/UX in order to provide a pleasant experience when creating websites.
+    
+      Some of my hobbies include calligraphy, drawing, exploring new music and watching organization/lifestyle videos on youtube.
+
+          
+ 		<section class="socials">
+      <a href="https://www.instagram.com/_mastering_my_creativity_/" target="_blank">
+        <img src="img/IG/sm-icons-instagram-glyph-logo.png" height="32px" alt="ig badge linking to www.instagram.com/_mastering_my_creativity_/">
+      </a>
+
+      <a href="https://www.github.com/alma-sanchez/" target="_blank">
+        <img src="img/GitHub-Mark/PNG/GitHub-Mark-32px.png" alt="github logo linking to my homepage at ">
+      </a> 
+      &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+
+      <a href="https://www.linkedin.com/in/almaysanchez/" target="_blank">
+        <img src="img/LinkedIn/In-only-logo/2-Color/In-2C-34px-TM.png" alt="linkedin badge linking to www.linkedin.com/in/almaysanchez/">
+      </a>
+      &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+
+      <a href="https://www.twitch.tv/msgiggleshits" target="_blank">
+        <img src="img/LinkedIn/In-only-logo/2-Color/In-2C-48px-TM.png" alt="linkedin badge linking to www.linkedin.com/in/almaysanchez/">
+      </a>
+      </section>
+
+
+    </div>
+
+
+    <footer>
+      &copy; 2021 Alma Sanchez
+    </footer>
+
+  </div>
+}
